286d0ec0ae30e3a6db7fcb74aeb82ee077e1c9ec
Erick G. Islas-Osuna <erickisos653@gmail.com>
2023-12-04 10:14:46 -0600

    wip add icarus definitions

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,26 +1,9 @@
-### 0.3.0
+# Change Log
 
-Add the following features:
-* Ask the user for a message to send when using the **Send message** command.
+All notable changes to the "icarus" extension will be documented in this file.
 
-Fix the following issues:
-* It was not possible to open an IRC channel if there was no active text editor.
+Check [Keep a Changelog](http://keepachangelog.com/) for recommendations on how to structure this file.
 
-### 0.2.1
+## [Unreleased]
 
-Fix the following issues:
-* Icon background was not fully transparent
-* The wrong message was displayed when an user parted the channel
-
-### 0.2.0
-
-Add the following features:
-* Ask the user for server address, port, channel and nick (instead of reading
-the configuration)
-* Display a message when an user part the channel
-* Display the server address as the tab title
-* Add an icon
-
-### 0.1.0
-
-Alpha release of **vscode-irc**. 
+- Initial release
--- src/extension.ts
+++ src/extension.ts
@@ -1,68 +1,18 @@
-'use strict';
+import * as vscode from 'vscode';
+import { IcarusSidebarProvider } from './services/IcarusSidebarProvider';
 
-import * as irc from 'irc';
-import { workspace, languages, window, commands, ExtensionContext, Disposable, InputBoxOptions } from 'vscode';  
-import ContentProvider, { generateUri } from './provider';
-import IrcInstance from './ircInstance';
-
-export function activate(context: ExtensionContext) {
-
-	const provider = new ContentProvider();
-	const providerRegistrations = Disposable.from(
-		workspace.registerTextDocumentContentProvider(ContentProvider.scheme, provider)
-	);
-
-	// Register command that crafts an uri
-	// Open the dynamic document, and shows it in the next editor
-	const openIrcCommandRegistration = commands.registerCommand('vscodeIrc.openIrc', () => {
-		let ircConfiguration = workspace.getConfiguration('irc');
-
-		askUserForIrcInstance().then(value => {
-			openIrcDocument(value);
-		});
-	});
-
-	const sendMessageCommandRegistration = commands.registerTextEditorCommand("vscodeIrc.sendMessage", editor => {  
-		// Get the URI of the active tab
-		// If we can get an IRC client from it, send a message!
-		var ircClient = new irc.Client();
-		ircClient = provider.getClientFromUri(editor.document.uri);
-
-		// Find the name of the channel in the JavaScript object
-		// TODO: this is quite ugly, maybe find a more elegant way
-		var channelName = ircClient.chans[Object.keys(ircClient.chans)[0]].key;
-
-		askUserForValue('Message', 'message').then(value => {
-			ircClient.say(channelName, value);  
-		})
-	});
+export function activate(context: vscode.ExtensionContext) {
+	const provider = new IcarusSidebarProvider(context.extensionUri);
 
 	context.subscriptions.push(
-		provider,
-		openIrcCommandRegistration,
-		sendMessageCommandRegistration, 
-		providerRegistrations
-	);
-}
+		vscode.window.registerWebviewViewProvider(IcarusSidebarProvider.viewType, provider));
 
-async function askUserForIrcInstance() {
-	var server = await askUserForValue('Server', 'server');
-	var port = await askUserForValue('Port', 'port');
-	var channel = await askUserForValue('Channel', 'channel (without the #)');
-	var nick = await askUserForValue('Nick', 'nick');
-	return new IrcInstance(server, port, channel, nick);
-}
+	context.subscriptions.push(
+		vscode.commands.registerCommand('icarus.openIcarus', () => {
+		}));
 
-function askUserForValue(prompt: string, placeholder: string) {
-	let options: InputBoxOptions = {
-		prompt: prompt,
-		placeHolder: placeholder
-	}
-
-	return window.showInputBox(options)
-}
-
-function openIrcDocument(ircInstance: IrcInstance) {
-	const uri = generateUri(ircInstance);
-	return workspace.openTextDocument(uri).then(doc => window.showTextDocument(doc));
+	// context.subscriptions.push(
+	// 	vscode.commands.registerCommand('calicoColors.clearColors', () => {
+	// 		provider.clearColors();
+	// 	}));
 }
